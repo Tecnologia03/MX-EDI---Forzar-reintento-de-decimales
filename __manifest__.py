b8a89c9ed99ba374ad26b8e62d49ccad1723f922
tecgbtek <tecnologia1@globalbtek.com>
2026-09-23 10:54:42 -0600

    listo

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Globalbtek
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MX EDI - Forzar reintento de decimales",
+    "summary": "Detección automática y reintento para CRP20268 y CRPER654 en complementos de pago",
+    "version": "18.0.1.0.0",
+    "author": "Globalbtek",
+    "license": "AGPL-3",
+    "depends": [
+        "account_edi",
+        "l10n_mx_edi",
+    ],
+    "data": [
+        "data/payment20_force_retry.xml",
+        "data/install_force_retry_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
